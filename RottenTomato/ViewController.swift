0abef344897ebb78a159fa89566842e3f6bfd3d1
Jiahang Scott Zou <scottjzou@gmail.com>
2015-02-08 12:42:31 -0800

    Initial Commit

---
+++ RottenTomato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RottenTomato
+//
+//  Created by Scott_Air on 15/2/8.
+//  Copyright (c) 2015年 ScottZou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
