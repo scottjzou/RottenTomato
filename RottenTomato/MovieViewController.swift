10094eb47c0ae52ccf4faa4d4741e673984996c0
Jiahang Scott Zou <scottjzou@gmail.com>
2015-02-08 14:09:38 -0800

    starting

---
+++ RottenTomato/MovieViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MovieViewController.swift
+//  RottenTomato
+//
+//  Created by Scott_Air on 15/2/8.
+//  Copyright (c) 2015年 ScottZou. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
