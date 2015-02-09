8842c44026f33afa3b94288ee9e542904216e218
Jiahang Scott Zou <scottjzou@gmail.com>
2015-02-08 16:32:52 -0800

    moviecell complete

---
+++ MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  RottenTomato
+//
+//  Created by Scott_Air on 15/2/8.
+//  Copyright (c) 2015年 ScottZou. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var synoposisLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- RottenTomato/MovieViewController.swift
+++ RottenTomato/MovieViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class MovieViewController: UIViewController {
+class MovieViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -23,7 +25,20 @@ class MovieViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as MovieCell
+        
+        cell.movieTitleLabel.text = "Title"
+        cell.synoposisLabel.text = "Synoposis"
+        
+//        cell.textLabel!.text = "Hello, I am at row: \(indexPath.row), section: \(indexPath.section)"
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
